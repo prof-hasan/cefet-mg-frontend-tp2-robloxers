878f68f8a123ccdfe7adc828241fcc8e415b33c8
saraeam <161386463+saraeam@users.noreply.github.com>
2025-01-31 13:14:51 -0300

    Add files via upload

---
+++ js/cartas.js
@@ -0,0 +1,19 @@
+let cartasEl = document.querySelectorAll('.cartas'), 
+    v[];
+
+function conferirDuplas(v[], num){ //ele confere se ja tem uma dupla com esse numero
+    let cont = 0;
+    for(let i = 0; i < v.length; i++) {
+        if(v[i] === num) cont++;
+        if(cont === 2) return true;
+    }
+    return false;
+}
+
+function criarDuplas(){
+    let numAleatorio = Math.ceil()
+    for (let i = 0; i < v.length; i++) {
+        Math
+        
+    }
+}
