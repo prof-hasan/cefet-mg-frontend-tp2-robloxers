82f277a1820a285d6b9d69f2d18b92d9bdf6e863
ThauanAraujo <162844198+ThauanAraujo@users.noreply.github.com>
2025-02-07 13:48:35 -0300

    Add files via upload

--- js/cartas.js
+++ js/cartas.js
@@ -1,9 +1,9 @@
-let cartasEl = document.querySelectorAll('.cartas'), 
-    v[];
+let cartasEl = document.querySelectorAll('.cartas');
+let duplasFormadas = [];
 
-function conferirDuplas(v[], num){ //ele confere se ja tem uma dupla com esse numero
+function conferirDuplas(v, num){ //ele confere se ja tem uma dupla com esse numero
     let cont = 0;
-    for(let i = 0; i < v.length; i++) {
+    for(let i = 0; i < v.length; i++) { 
         if(v[i] === num) cont++;
         if(cont === 2) return true;
     }
@@ -11,9 +11,30 @@ function conferirDuplas(v[], num){ //ele confere se ja tem uma dupla com esse nu
 }
 
 function criarDuplas(){
-    let numAleatorio = Math.ceil()
-    for (let i = 0; i < v.length; i++) {
-        Math
+    let numAleatorio;
+
+    for (let i = 0; i < cartasEl.length; i++) {
+        numAleatorio = Math.floor(Math.random() * 4) + 1;
+
+        if (!conferirDuplas(duplasFormadas, numAleatorio)) {
+            cartasEl[i].classList.add('c' + numAleatorio);
+            duplasFormadas.push(numAleatorio);
+            //cartasEl[i].classList.remove('virada');
+
+        } else {
+            while (conferirDuplas(duplasFormadas, numAleatorio)) {
+                numAleatorio = Math.floor(Math.random() * 4) + 1;
+
+                if (!conferirDuplas(duplasFormadas, numAleatorio)) {
+                    cartasEl[i].classList.add('c' + numAleatorio);
+                    //cartasEl[i].classList.remove('virada');
+                    break;
+                }
+            }
+            duplasFormadas.push(numAleatorio);
+        }
         
     }
 }
+
+criarDuplas();
