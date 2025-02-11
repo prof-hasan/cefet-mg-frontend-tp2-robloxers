da9aa7ef6940b5a5dc68a57751b907b799c379c2
veronicaalvesde <161507194+veronicaalvesde@users.noreply.github.com>
2025-02-11 17:23:50 -0300

    Add files via upload

--- js/temas.js
+++ js/temas.js
@@ -4,7 +4,6 @@ const botoesEl = document.querySelectorAll('.botao-escolha');
 for(let botao in botoesEl){
     botao.addEventListener('click', () => {
         let classeNova = botao.getAttribute('value');
-        bodyEl.style.backgroundImage = '';
-        bodyEl.classList.toggle(classeNova);
+        bodyEl.classList.add('' + classeNova + '');
     });
 }
